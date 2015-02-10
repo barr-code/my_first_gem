accd85bf6852fed6da760294838ce753c0a376cf
barr-code <vb329@georgetown.edu>
2015-02-10 11:57:15 -0500

    Formatted time output

--- lib/my_first_gem.rb
+++ lib/my_first_gem.rb
@@ -2,6 +2,7 @@ require "my_first_gem/version"
 
 module MyFirstGem
   def self.tell_time(time = Time.now)
-    return "It's #{time}."
+    time_formatted = time.strftime("%I:%M")
+    return "It's #{time_formatted}."
   end
 end
