c5581a1b2426e53f94b5db3a50a8ee92ad75f665
barr-code <vb329@georgetown.edu>
2015-02-11 20:31:42 -0500

    Added morning message

--- lib/my_first_gem.rb
+++ lib/my_first_gem.rb
@@ -5,4 +5,8 @@ module MyFirstGem
     time_formatted = time.strftime("%I:%M")
     return "It's #{time_formatted}."
   end
+
+  def self.message(time)
+    return "Good morning. #{self.tell_time(time)}"
+  end
 end
--- spec/my_first_gem_spec.rb
+++ spec/my_first_gem_spec.rb
@@ -4,4 +4,9 @@ describe MyFirstGem do
   it 'tells the time' do
     expect(MyFirstGem.tell_time).to eq "It's #{Time.now.strftime("%I:%M")}."
   end
+
+  it 'has a message for morning' do
+    time = Time.new(2015, 1, 1, 9)
+    expect(MyFirstGem.message(time)).to eq "Good morning. It's 09:00."
+  end
 end
