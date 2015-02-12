78478fb37dcd49184af3c3233d7e678e4d3093a6
barr-code <vb329@georgetown.edu>
2015-02-11 20:41:11 -0500

    Added afternoon message

--- lib/my_first_gem.rb
+++ lib/my_first_gem.rb
@@ -7,6 +7,7 @@ module MyFirstGem
   end
 
   def self.message(time)
-    return "Good morning. #{self.tell_time(time)}"
+    return "Good morning. #{self.tell_time(time)}" if time.hour > 5 && time.hour < 12
+    return "Good afternoon. #{self.tell_time(time)}" if time.hour > 12 && time.hour < 18
   end
 end
--- spec/my_first_gem_spec.rb
+++ spec/my_first_gem_spec.rb
@@ -9,4 +9,9 @@ describe MyFirstGem do
     time = Time.new(2015, 1, 1, 9)
     expect(MyFirstGem.message(time)).to eq "Good morning. It's 09:00."
   end
+
+  it 'has a message for afternoon' do
+    time = Time.new(2015, 1, 1, 13)
+    expect(MyFirstGem.message(time)).to eq "Good afternoon. It's 01:00."
+  end
 end
